c89e510677609b47f060a5ae52b79c2e52c8992e
mssalvador <michael.svanholm@gmail.com>
2017-05-18 15:55:26 +0200

    buttons created for select clusters. Test made for compute distance

--- shared/ComputeDistances.py
+++ shared/ComputeDistances.py
@@ -6,7 +6,8 @@ Created on May 15, 2017
 import numpy as np
 import math
 
-def compute_distance(point,center):
+
+def compute_distance(point, center):
     '''
     Computes the euclidean  distance from a data point to the cluster center.
     
@@ -17,7 +18,10 @@ def compute_distance(point,center):
     assert isinstance(point, np.ndarray), str(point)+" is not an a numpy array"
     assert isinstance(center, np.ndarray), str(center)+" is not an a numpy array"
 
-    squared_dist = np.dot((center-point),(center-point))
+    squared_dist = np.dot((center-point), (center-point))
 
     return math.sqrt(squared_dist)
 
+
+def subtract_points(point, center):
+    pass

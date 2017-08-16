303cc47cb0982b865029466dc02433bb1da401a9
mssalvador <michael.svanholm@gmail.com>
2017-08-16 13:54:26 +0200

    Added ComputeDataFrameSize

--- sample/main.py
+++ sample/main.py
@@ -34,41 +34,27 @@ TEST_DICT = {'features': ('AarsVaerk_1','AarsVaerk_2','AarsVaerk_3'),
 
 if __name__ == '__main__':
 
-    if os.path.exists('jobs.zip'):
-        sys.path.insert(0, 'jobs.zip')
+    if os.path.exists('shared.zip'):
+        sys.path.insert(0, 'shared.zip')
     else:
-        sys.path.insert(0, './jobs')
+        sys.path.insert(0, './shared')
 
-    # sc = SparkContext.getOrCreate()
-    # dd = DummyData(sc)
-    #
-    # # examples on splitting
-    # ddx = dd.df[dd.df["x"] > 0.5]
-    # ddx.show(5)
-    #
-    # # slicing a data frame is as follows
-    #
-    # ddy = dd.df["label", "x"]
-    # ddy.show(5)
-    # labels = ['header_1', 'header_2', 'header_3']
-    # features = ['feature_1', 'feature_2', 'feature_3', 'feature_4']
-    # df_outliers = create_dummy_data(number_of_samples=1000000,
-    #                                 labels=labels,
-    #                                 features=features,
-    #                                 outlier_factor=50,
-    #                                 outlier_number=0.5
-    #                                 )
-    # df_outliers.show()
-    # print(df_outliers.count())
+    if os.path.exists('sample.zip'):
+        sys.path.insert(0,'sample.zip')
+    else:
+        sys.path.insert(0, './sample')
 
-    # df = make_outliers(df_outliers, 0.5, 100)
-    # features_of_list = map(lambda f: '(' + str(f) + ' > 1)', [i[0] for i in df_outliers.dtypes if i[1] == 'double'])
-    # print('Number of outliers = ' + str(df_outliers.where(' or '.join(features_of_list)).count()))
-    # #df.show()
-    # df_outliers.write.parquet(PARQUET_PATH+'outlier_df.parquet', mode='overwrite')
+    from shared.create_dummy_data import create_dummy_data
+    from shared.ComputeDataFrameSize import compute_size_of_dataframe
 
-    from classification.ExecuteClassificationWorkflow import ExecuteWorkflowClassification
+    n = int(sys.argv[1])
+    if not sys.argv[1]:
+        n = 100000
 
-    ex = ExecuteWorkflowClassification()
-
-    ex.create_custom_pipeline()
+    #help(create_dummy_data)
+    df = create_dummy_data(number_of_samples=n,
+                           feature_names=["x", "y", "z"],
+                           label_names=["label"],
+                           outlier_factor=10,
+                           outlier_number=0.1)
+    compute_size_of_dataframe(df)

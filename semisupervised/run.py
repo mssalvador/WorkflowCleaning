3e9acbeaeef70df41444753aac322f2b3264b8bf
mssalvador <michael.svanholm@gmail.com>
2017-12-13 16:21:17 +0100

    changes to run in semisupervised. Data has a corruption...

--- semisupervised/run.py
+++ semisupervised/run.py
@@ -2,6 +2,10 @@ from semisupervised.LabelPropagation import label_propagation
 from shared.WorkflowLogger import logger_info_decorator, logger
 from pyspark.sql import SparkSession
 from pyspark.sql import types as T
+import functools
+
+default_lp_param = {'sigma': 0.42, 'tol':0.01, 'k': 2, 'max_iters': 5,'eval_type': 'max' }
+
 
 @logger_info_decorator
 def run(sc, **kwargs):
@@ -11,21 +15,23 @@ def run(sc, **kwargs):
     spark.conf.set("spark.sql.crossJoin.enabled", "true")
     input_data = kwargs.get('input', None)
     feature_columns = [T.StructField(f, T.DoubleType(), False) for f in kwargs.get('features', None)]
-    label_columns = [T.StructField(kwargs.get('labels', None)[0], T.IntegerType(), True)]
-    id_column = [T.StructField(kwargs.get('id', 'id'), T.IntegerType(), False)]
-    algo_types = kwargs.get('algo_params', None)
+    label_columns = [T.StructField(kwargs.get('labels', None), T.IntegerType(), True)]
+    id_column = [T.StructField(idx, T.IntegerType(), False) for idx in kwargs.get('id', 'id')]
+    algo_types = kwargs.get('algo_params', {})
+    for key in default_lp_param.keys():
+        if key not in algo_types:
+            algo_types[key] = default_lp_param[key]
 
     #Import data
     input_data_frame = spark.read.load(
-        path=input_data, format='csv', schema=id_column+label_columns+feature_columns)
+        path=input_data, format='csv', schema=T.StructType(id_column+label_columns+feature_columns))
 
     # Execute algorithm
-    output_data_frame = label_propagation(
-        sc=sc, data_frame=input_data_frame, label_col=kwargs.get('labels', None),
-        id_col=kwargs.get('id', 'id'), feature_cols=kwargs.get('features', None),
-        k=algo_types['k'], sigma=algo_types['sigma'], max_iters=algo_types['max_iter'],
-        tol=algo_types['tol'], standardize=algo_types['standardizer'],
-        eval_type=algo_types['eval'], priors=algo_types['prior'])
+    partial_lp = functools.partial(
+        label_propagation, sc=sc, data_frame=input_data_frame,
+        label_col=kwargs.get('labels', None), id_col=kwargs.get('id', 'id'),
+        feature_cols=kwargs.get('features', None))
+    output_data_frame = partial_lp(**algo_types)
 
     # Return result
     return output_data_frame

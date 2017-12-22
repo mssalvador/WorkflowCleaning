fcf0a4d019102cf1695730a23d28b64809573b73
mssalvador <michael.svanholm@gmail.com>
2017-12-22 10:07:50 +0100

    new changes

--- examples/SemisupervisedMnist.py
+++ examples/SemisupervisedMnist.py
@@ -1,5 +1,12 @@
 import shared.create_dummy_data as ccd
+import semisupervised as ss
+import pyspark
 
-def mnist():
-    train_pdf, test_pdf = ccd.load_mnist()
+def mnist(sc : pyspark.SparkContext, **kwargs):
+    # This will be the mnist performance test module
+    spark_session = pyspark.sql.SparkSession(sparkContext=sc)
+    train_pdf, test_pdf = ccd.load_mnist(sc, **kwargs)
+    # train_df = spark_session.createDataFrame(train_pdf)
+    # test_df = spark_session.createDataFrame(test_pdf)
+    # ss.label_propagation(sc=sc, data_frame=train_df,)
     return train_pdf, test_pdf

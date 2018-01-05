d8cc6d8b787f62844315143a8f0e18e3e214819a
mssalvador <michael.svanholm@gmail.com>
2018-01-05 15:09:31 +0100

    LP_Graph Matrixfied

--- semisupervised/LP_Graph.py
+++ semisupervised/LP_Graph.py
@@ -1,17 +1,16 @@
 import numpy as np
-import pyspark
-from pyspark.sql import types as T
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import VectorAssembler, StandardScaler
-from pyspark.ml.linalg import SparseVector
+from pyspark.mllib.linalg import distributed
 from pyspark.sql import functions as F
+from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.ml.linalg import SparseVector, VectorUDT
 from shared import ConvertAllToVecToMl
+from pyspark.ml import Pipeline
 from shared.WorkflowLogger import logger_info_decorator
 
 
 @logger_info_decorator
 def _compute_weights(vec_x, vec_y, sigma):
-    if isinstance(vec_y,SparseVector) | isinstance(vec_x, SparseVector):
+    if isinstance(vec_y, SparseVector) | isinstance(vec_x, SparseVector):
         x_d = vec_x.toArray()
         y_d = vec_y.toArray()
         return float(np.exp(-(np.linalg.norm(x_d-y_d, ord=2)**2)/sigma**2))
@@ -20,37 +19,28 @@ def _compute_weights(vec_x, vec_y, sigma):
 
 
 @logger_info_decorator
-def create_complete_graph(data_frame, feature_columns, id_column='id',
+def create_complete_graph(sc, data_frame, feature_columns, id_column='id',
                           label_column='label', standardize=True, sigma=0.7):
+    std_feature_name = 'standard_feat'
     feature_gen = VectorAssembler(inputCols=feature_columns, outputCol='features')
-    vector_converter = ConvertAllToVecToMl.ConvertAllToVecToMl(
-        inputCol=feature_gen.getOutputCol(), outputCol='conv_feat')
-    standardizer = StandardScaler(withMean=False, withStd=False,
-        inputCol=vector_converter.getOutputCol(), outputCol='standard_feat')
-
-    if standardize:
-        standardizer.setWithMean(True)
-        standardizer.setWithStd(True)
-
-    pipeline = Pipeline(stages=[feature_gen, vector_converter, standardizer])
+    converter = ConvertAllToVecToMl.ConvertAllToVecToMl(inputCol='features', outputCol='converteds')
+    standardize = StandardScaler(withMean=standardize, withStd=standardize,
+        inputCol=converter.getOutputCol(), outputCol=std_feature_name)
+    pipeline = Pipeline(stages=[feature_gen, converter, standardize])
     model = pipeline.fit(data_frame)
 
-    rdd = (model.transform(data_frame)
-        .select(id_column, label_column, F.col(standardizer.getOutputCol()).alias('features'))
-        .rdd
-        .repartition(10)
-        .cache()
-    )
-    print(rdd.getNumPartitions())
-    output_column_types = [
-        ('row', T.IntegerType(), False), ('row_label', T.DoubleType(), True),
-        ('column', T.IntegerType(), False), ('column_label', T.DoubleType(), True),
-        ('weights_ab', T.DoubleType(), False)]
-    schema = T.StructType([T.StructField(*i) for i in output_column_types])
+    vector_df = model.transform(data_frame)
+    to_sparse_udf = F.udf(lambda x: SparseVector(len(x), [(i,j) for i,j in enumerate(x) if j != 0]), VectorUDT())
+    standard_X_sparse = vector_df.withColumn('weights', to_sparse_udf(F.col(std_feature_name)))
+    bc_vec = sc.broadcast(standard_X_sparse.select(id_column,'weights').rdd.collectAsMap())
 
-    cartesian_rows = pyspark.Row(list(map(lambda x: x[0], output_column_types)))
-    second_rdd = rdd.cartesian(rdd)#.filter(lambda x: x[0][0] < x[1][0])
-    distance_rdd = second_rdd.map(lambda v: cartesian_rows(
-        v[0][id_column], v[0][label_column], v[1][id_column], v[1][label_column],
-        _compute_weights(vec_x=v[0]['features'], vec_y=v[1]['features'], sigma=sigma)))
-    return distance_rdd.toDF(schema=schema)
+    rdd_srink = standard_X_sparse.rdd.map(lambda x: (x[id_column], x[label_column]))
+    rdd_cartesian = (rdd_srink
+                     .cartesian(rdd_srink)
+                     .map(lambda x: (*x[0], *x[1]))
+                     .map(lambda x: distributed.MatrixEntry(
+                         x[0], x[2], _compute_weights(bc_vec.value.get(x[0]),
+                                                      bc_vec.value.get(x[2]),
+                                                      sigma=sigma)))
+                    )
+    return distributed.CoordinateMatrix(rdd_cartesian)

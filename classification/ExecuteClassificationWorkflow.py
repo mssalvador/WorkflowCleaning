9131bc6ca39a8e9ffa15cd04c77a2d5994ba3187
mssalvador <michael.svanholm@gmail.com>
2018-04-24 16:24:16 +0200

    recommented ConvertAllToVecToMl

--- classification/ExecuteClassificationWorkflow.py
+++ classification/ExecuteClassificationWorkflow.py
@@ -21,21 +21,25 @@ import numpy as np
 class ExecuteWorkflowClassification(object):
 
     #@logger_info_decorator
-    def __init__(self, dict_params=None, standardize=False, featureCols=None):
+    def __init__(self, dict_params=None, standardize=False, feature_cols=None, label_col=None, id_col=None):
         '''
         Constructor for ExecuteWorkflowClassification.
         :param dict_params:
         :param standardize:
-        :param featureCols:
+        :param feature_cols:
         :param labelCols:
         '''
-        self._algorithm = dict_params.pop('algorithm', 'LogisticRegression')
+        self._algorithm = ExecuteWorkflowClassification._check_algorithm(
+            algorithm=dict_params.pop('algorithm', 'LogisticRegression')
+        )
         self._params = dict_params
         self._standardize = standardize
-        self._featureCols = featureCols
-
+        self._feature_cols = feature_cols
+        self._label_col = label_col
+        self._id_col = id_col
+        self._pipeline = None
         try:
-            self._pipeline, self._parameter_grid = self.create_standard_pipeline()
+            self._pipeline = self.create_standard_pipeline()
         except TypeError as te:
             tb = sys.exc_info()[2]
             print(te)
@@ -59,6 +63,15 @@ class ExecuteWorkflowClassification(object):
     def __repr__(self):
         return "ExecuteWorkflowClassification('{}')".format(self._params)
 
+    # @logger_info_decorator
+    @staticmethod
+    def _check_algorithm(algorithm='LogisticRegression'):
+        try:
+            applicable_algos = {'logisticregression': 'LogisticRegression'}
+            return applicable_algos[algorithm.lower()]
+        except AttributeError as ae:
+            return 'LogisticRegression'
+
     def create_custom_pipeline(self):
 
         '''
@@ -74,17 +87,17 @@ class ExecuteWorkflowClassification(object):
         all_transformation = filter(lambda x: "Model" not in x, feature.__all__)
         print("\n".join(all_transformation))
 
-    def create_standard_pipeline(self):
+    def create_standard_pipeline(self, cross_validate=False):
         '''
         This method creates a standard pipeline, standard meaning: vectorize, standardize and model...
         :return: Pipeline for pyspark, ParameterGrid for Pyspark pipeline
         '''
 
         # Feature columns are created from instance variables
-        feature_columns = [i.name for i in self._featureCols]
+        # feature_columns = [i.name for i in self._feature_cols]
 
         # Vectorized transformation
-        vectorizer = VectorAssembler(inputCols=feature_columns, outputCol='v_features')
+        vectorizer = VectorAssembler(inputCols=self._feature_cols, outputCol='v_features')
         # Cast the vector from mllib to ml
         converter = ConvertAllToVecToMl(inputCol=vectorizer.getOutputCol(), outputCol='casted')
         # Standardize estimator
@@ -94,23 +107,23 @@ class ExecuteWorkflowClassification(object):
         )
         # Labels and strings are already set into the model, +
         dict_parameters = dict(filter(lambda x: not isinstance(x[1], tuple), self._params.items()))
-        dict_features = dict(ExecuteWorkflowClassification.generate_equidistant_params(self._params))
         dict_parameters['featuresCol'] = standardizes.getOutputCol()
+        dict_parameters['labelCol'] = self._label_col[0] # HACK!!!
         #print(label_dict)
 
         # Model is set
         model = eval("classification." + self._algorithm)(**dict_parameters)
 
-        # Parameter is set
+        pipe = Pipeline(stages=[vectorizer, converter, standardizes, model])
+        return pipe
+
+    def create_grid(self, dict_features):
         param_grid = tuning.ParamGridBuilder()
         for model_parameter, grid_values in dict_features.items():
             if isinstance(grid_values, int) or isinstance(grid_values, float):
-                param_grid.baseOn(eval('model.'+model_parameter), grid_values)
+                param_grid.baseOn(eval('model.' + model_parameter), grid_values)
             else:
-                param_grid.addGrid(eval('model.'+model_parameter), grid_values)
-
-        pipe = Pipeline(stages=[vectorizer, converter, standardizes, model])
-        return pipe, param_grid.build()
+                param_grid.addGrid(eval('model.' + model_parameter), grid_values)
 
     @logger_info_decorator
     def run_cross_val(self, data, evaluator, n_folds):
@@ -147,14 +160,5 @@ class ExecuteWorkflowClassification(object):
                 yield (key, np.linspace(val[0], val[1], number_of_spaces, True, dtype=int))
             else:
                 yield (key, np.linspace(val[0], val[1], number_of_spaces, True, dtype=float))
-
-    @logger_info_decorator
-    def run_pipeline(self, data):
-        """
-        This method should execute the pipeline.
-        :param data:
-        :return:
-        """
-        return self._pipeline(data)
 
 
